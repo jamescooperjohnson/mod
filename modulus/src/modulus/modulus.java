12f3fd4113a804edfc40cb193b444f5b3cde05f7
james.johnson <james.johnson@MU-C1-18.mullen.pvt.k12.co.us>
2016-08-25 09:09:40 -0600

    not close

--- modulus/src/modulus/modulus.java
+++ modulus/src/modulus/modulus.java
@@ -1,12 +1,25 @@
 package modulus;
 
+import java.util.Scanner;
+
+
+
 public class modulus
 	{
-
-		public static void main(String[] args)
+public static void main(String[] args)
+	{
+	Scanner userInput = new Scanner (System.in);	
+	System.out.println("Give me a number");
+	int number = userInput.nextInt();
+	
+		if (number % 2 == 1);
 			{
-				// TODO Auto-generated method stub
-
+			System.out.println("Your number is odd");
 			}
-
+		else 
+				{
+				System.out.println("Your number is Even");
+			}
+	
 	}
+}
